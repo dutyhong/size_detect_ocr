ba8c6b29bf09d1c9c00091660c66329343b9c0b6
duty <tizi@fordeal.com>
2022-01-12 12:25:07 +0800

    修改路径

--- ocr/urls.py
+++ ocr/urls.py
@@ -23,5 +23,6 @@ urlpatterns = [
     # url(r'^name/', include('sizechart_detect.urls'), name='sizechart_detect'),
     # url(r'^name/', include('sizechart_detect.urls'))
     # path('sizechart_detect/',include('sizechart_detect.urls')),
-    path('detect/',views.post, name='post'),
+    path('admin/', admin.site.urls),
+    # path('detect/',views.post, name='post'),
 ]
