4d48feb7a92bc7911c6f42aae7f9b6ea56f24bd4
duty <tizi@fordeal.com>
2022-01-12 14:47:27 +0800

    修改路径

--- sizechart_detect/global_config.py
+++ sizechart_detect/global_config.py
@@ -2,7 +2,7 @@
 # 创建时间 ：2022/1/11 9:54 上午
 # 文件 ：config.py
 import os
-APP_ENV = os.environ.get('APP_ENV', 'default').lower()
+APP_ENV = os.environ.get('APP_ENV')
 KAFKA_SERVER = "gz-kafka-dev-inner.duolainc.com:9092" if APP_ENV=="default" else "xxx"
 ORIGINAL_IMAGE_FILEPATH = '/Users/duty/PycharmProjects/ocr/data/image_data/original_images' if APP_ENV=="default" else "/home/tizi/ocr/data/image_data/original_images"
 CTPN_MODELS_DIR = '/Users/duty/PycharmProjects/ocr/data/models/' if APP_ENV=="default" else "/home/tizi/ocr/data/models/"
