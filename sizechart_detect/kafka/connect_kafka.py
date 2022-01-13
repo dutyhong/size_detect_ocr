5b2b1032569357f1addc1764ca3b74bddd108769
duty <tizi@fordeal.com>
2022-01-13 19:54:55 +0800

    ttt

--- sizechart_detect/kafka/connect_kafka.py
+++ sizechart_detect/kafka/connect_kafka.py
@@ -5,4 +5,6 @@ from kafka import KafkaConsumer, KafkaProducer
 
 from sizechart_detect.global_config import KAFKA_SERVER, KAFKA_CONSUMER_TOPIC
 kafka_producer = KafkaProducer(bootstrap_servers=KAFKA_SERVER, api_version=(0,10))
-kafka_consumer = KafkaConsumer(KAFKA_CONSUMER_TOPIC, bootstrap_servers=[KAFKA_SERVER], group_id='image', auto_offset_reset='latest', consumer_timeout_ms=1000*60)
+print("生产者连接完成")
+kafka_consumer = KafkaConsumer(KAFKA_CONSUMER_TOPIC, bootstrap_servers=KAFKA_SERVER, group_id='image', auto_offset_reset='latest', consumer_timeout_ms=1000*60)
+print("消费者连接完成")
