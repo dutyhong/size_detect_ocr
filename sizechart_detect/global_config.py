0e366e97c0d0ba6df88d77e836052dfbb70195f8
duty <tizi@fordeal.com>
2022-01-13 19:50:32 +0800

    加入线上kafka地址

--- sizechart_detect/global_config.py
+++ sizechart_detect/global_config.py
@@ -3,7 +3,7 @@
 # 文件 ：config.py
 import os
 APP_ENV = os.environ.get('APP_ENV')
-KAFKA_SERVER = "gz-kafka-dev-inner.duolainc.com:9092" if APP_ENV=="default" else "xxx"
+KAFKA_SERVER = "gz-kafka-dev-inner.duolainc.com:9092" if APP_ENV=="default" else "ir-search-kafka01.duolainc.com:9092,ir-search-kafka02.duolainc.com:9092,ir-search-kafka03.duolainc.com:9092"
 KAFKA_PRODUCER_TOPIC = "size_recog_result"
 KAFKA_CONSUMER_TOPIC = "image_recognize"
 ORIGINAL_IMAGE_FILEPATH = '/Users/duty/PycharmProjects/ocr/data/image_data/original_images' if APP_ENV=="default" else "/home/tizi/ocr/data/image_data/original_images"
