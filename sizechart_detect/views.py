69fbf5f1f99467a93e1aeb84ab34ea3ab6211e9f
duty <tizi@fordeal.com>
2022-03-21 13:07:34 +0800

    修改路径

--- sizechart_detect/views.py
+++ sizechart_detect/views.py
@@ -97,5 +97,5 @@ def kafka_post(request):
 			except Exception:
 				logger.error(traceback.format_exc())
 				logger.error("MSG:处理出错")
-			# finally:
-			# 	kafka_consumer.close()
+			finally:
+				kafka_consumer.close()
